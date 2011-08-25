5dd64c83051191406ead59dcef47963dd8a50f9f
cargnelutti.lucas@gmail.com <cargnelutti.lucas@gmail.com@1da7aa7e-b3b5-7151-2798-f9632487c735>
2011-08-25 23:41:41 +0000

    Borre todas los comentarios..

--- trunk/TSB_TP1/src/tsb_tp1/Main.java
+++ trunk/TSB_TP1/src/tsb_tp1/Main.java
@@ -1,23 +1,10 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tsb_tp1;
 
-/**
-<<<<<<< .mine
- *hola hola
-=======
- *hhaaaaaaaaaaaaaaaaaaaa
- * por dios como ago para cmpartir..?
->>>>>>> .r4
- * @author Ale
- */
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         System.out.println("Hola la concha");
         Prueba1 p= new Prueba1();
