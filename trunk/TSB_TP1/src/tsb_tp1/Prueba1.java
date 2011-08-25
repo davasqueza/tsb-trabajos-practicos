a7aa58fef0dc659575b602af48cbd368a7a863da
cargnelutti.lucas@gmail.com <cargnelutti.lucas@gmail.com@1da7aa7e-b3b5-7151-2798-f9632487c735>
2011-08-25 23:48:37 +0000

    Borre todas los comentarios.. Borro de nuevo los comentarios de prueba..

--- trunk/TSB_TP1/src/tsb_tp1/Prueba1.java
+++ trunk/TSB_TP1/src/tsb_tp1/Prueba1.java
@@ -1,13 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tsb_tp1;
 
-/**
- *
- * @author Franco
- */
+
 public class Prueba1 {
     private int Dni;
     
